ae80fc08cbfd2ae208f4a09ff4358b40699ed9b5
Hesto <losiakp@gmail.com>
2016-09-16 11:36:02 +0200

    service provider

---
+++ src/SchedulerServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Hesto\LaravelScheduler;
+
+use Illuminate\Support\ServiceProvider;
+
+class SchedulerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //$this->registerInstallCommand();
+    }
+
+    /**
+     * Register the scheduler:install command.
+     */
+    private function registerInstallCommand()
+    {
+        $this->app->singleton('command.hesto.scheduler.install', function ($app) {
+            return $app['Hesto\LaravelScheduler\Commands\SchedulerInstallCommand'];
+        });
+
+        $this->commands('command.hesto.scheduler.install');
+    }
+}
